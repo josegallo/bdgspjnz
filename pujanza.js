c60b412d0cfef1bff6833ce5a020b32fe61b4d61
josegallo <josegallo@gmail.com>
2019-10-14 12:57:54 +0200

    change of string Indice to index working only on english pages

--- pujanza.js
+++ pujanza.js
@@ -1,23 +1,20 @@
 jQuery(function($){ 
-    console.log(top.location.pathname);
-    var urlToParse = top.location.pathname.substring(0, 3);
-    console.log(urlToParse);
-    if (urlToParse === '/en/') {
-        console.log('this is type the page to change index');
-        var indexToChange = $('div#ez-toc-container p.ez-toc-title ');
-        indexToChange.text("Index");
-        // console.log(indexToChange.text());
-    }
-});
 
-//for changing correctly specific pages
-jQuery(function($){ ls
     console.log(top.location.pathname);
-    if (top.location.pathname === '/en/lorem-ipsum/') {
-        console.log('this is the page');
+    var UrlToInspect = top.location.pathname;
+    console.log(UrlToInspect.substring(0,4));
+    var substringUrlToCheck = UrlToInspect.substring(0,4).toString();
+    console.log (substringUrlToCheck);
+    console.log (typeof substringUrlToCheck );
+    var urlEnglishLength = top.location.pathname.length;
+    console.log(urlEnglishLength);
+    
+    if (urlEnglishLength >= 4 && substringUrlToCheck == '/en/') {
+        // console.log ('it has at least 4 letters and it is english');
         var indexToChange = $('div#ez-toc-container p.ez-toc-title ');
         indexToChange.text("Index");
-        // console.log(indexToChange.text());
+    } else {
+        console.log ('no');
     }
-});
     
+});
