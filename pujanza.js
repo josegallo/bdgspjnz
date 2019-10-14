034db6c5b953e422d5f4ff6f8db0631ba5d66663
josegallo <josegallo@gmail.com>
2019-10-14 13:08:09 +0200

    code depurated

--- pujanza.js
+++ pujanza.js
@@ -1,11 +1,6 @@
 jQuery(function($){ 
-
-    console.log(top.location.pathname);
     var UrlToInspect = top.location.pathname;
-    console.log(UrlToInspect.substring(0,4));
     var substringUrlToCheck = UrlToInspect.substring(0,4).toString();
-    console.log (substringUrlToCheck);
-    console.log (typeof substringUrlToCheck );
     var urlEnglishLength = top.location.pathname.length;
     console.log(urlEnglishLength);
     
@@ -13,8 +8,5 @@ jQuery(function($){
         // console.log ('it has at least 4 letters and it is english');
         var indexToChange = $('div#ez-toc-container p.ez-toc-title ');
         indexToChange.text("Index");
-    } else {
-        console.log ('no');
-    }
-    
+    } 
 });
