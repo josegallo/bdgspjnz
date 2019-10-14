1007045b16cfea9b96c45ad8eadcc246d3fc047d
josegallo <josegallo@gmail.com>
2019-10-14 18:39:47 +0200

    refactoring for theme

--- pujanza.js
+++ pujanza.js
@@ -2,7 +2,6 @@ jQuery(function($){
     var UrlToInspect = top.location.pathname;
     var substringUrlToCheck = UrlToInspect.substring(0,4).toString();
     var urlEnglishLength = top.location.pathname.length;
-    console.log(urlEnglishLength);
     
     if (urlEnglishLength >= 4 && substringUrlToCheck == '/en/') {
         // console.log ('it has at least 4 letters and it is english');
